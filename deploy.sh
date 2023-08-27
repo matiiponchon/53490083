efb196f0e1e8402007169ac47c8018df1c80662f
florenciayafar <69212377+florenciayafar@users.noreply.github.com>
2023-08-27 20:59:08 -0300

    intuitivo-challenge

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,43 @@
 #!/bin/bash
 
 echo "Deploy."
+
+name: Add Branch Protections
+
+on:
+  push:
+    branches:
+      - main  
+
+jobs:
+  branch_protections:
+    runs-on: ubuntu-latest
+    
+    steps:
+    - name: Checkout code
+      uses: actions/checkout@v2
+
+    - name: Add Branch Protections
+      env:
+        BRANCH_NAME: "main"  
+      run: |
+        curl -u ${{ secrets.GITHUB_USER }}:${{ secrets.GITHUB_TOKEN }} \
+          -X PUT \
+          -H "Accept: application/vnd.github.v3+json" \
+          https://api.github.com/repos/${{ github.repository }}/branches/${BRANCH_NAME}/protection \
+          -d '{
+            "required_status_checks": {
+              "strict": true,
+              "contexts": []
+            },
+            "enforce_admins": true,
+            "required_pull_request_reviews": {
+              "dismissal_restrictions": {
+                "users": [],
+                "teams": []
+              },
+              "dismiss_stale_reviews": true,
+              "require_code_owner_reviews": true
+            },
+            "restrictions": null
+          }'
---
+++ java_team_scripts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Coloca aquí los comandos para ejecutar los scripts del equipo java-team
+javac scripts/java_script_1.java
+java scripts.java_script_1
